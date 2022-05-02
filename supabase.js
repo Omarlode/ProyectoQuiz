df97f24453a9394430c354b0dbbdc2aebd5971e3
Omar <omarhlode@gmail.com>
2022-05-02 12:59:41 +0100

    boton sigiente pregunta

--- supabase.js
+++ supabase.js
@@ -5,16 +5,20 @@ const mySupabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2Z3hzcnZkbmVtdG9vZWNxdm51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTE0ODI4MjUsImV4cCI6MTk2NzA1ODgyNX0.JGEWmvd8q5InCfWgT-lfyUlDo0hSbHMnqBDsTHhYs7c"
 );
 
-let currentP = 2; //posicion actual del array
-
+let currentP = 0; //posicion actual del array
+let allQuestions;
 async function loadData() {
   const { data, error } = await mySupabase.from("questions").select();
   //console.log(data);
   //console.log(error);
+  allQuestions = data;
+  setQuestion();
+}
 
-  document.getElementById("h1").innerText = data[currentP].title;
+function setQuestion() {
+  document.getElementById("h1").innerText = allQuestions[currentP].title;
 
-  let answers = data[currentP].answers;
+  let answers = allQuestions[currentP].answers;
   let result = "";
 
   answers.forEach((element, index) => {
@@ -27,6 +31,11 @@ async function loadData() {
     //console.log(index);
   });
   document.getElementById("myAnswers").innerHTML = result;
+}
+function nextQuestion() {
+  console.log("dale!!");
+  currentP += 1;
+  setQuestion();
 }
 
 loadData();
